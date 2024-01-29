ef7d237003be945769a72786ee38acd0ad30f5fd
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 19:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let user = navigator.userAgent;
+let user2 =navigator.appName;
+
+let user3 = navigator.appVersion;
+
+
+
+const display = document.getElementById("browser-info");
+display.innerText = "You are using " +user2 +" version "+user3;
